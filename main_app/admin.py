6effb9545fae840594dfdc9ee4403ea9c9ef7106
mileiny1 <mileinygreys@gmail.com>
2025-07-24 22:04:53 -0400

    another part

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Cat
 
 # Register your models here.
+
+
+
+admin.site.register(Cat)
--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,29 @@
-from django.shortcuts import render
-from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Cat
+# Home view
+
 
 def home(request):
-    # Send a simple HTML response
-    return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
-# Create about view function
+    return render(request, 'home.html')
+
+# About view
 def about(request):
     return render(request, 'about.html')
+
+
+
+# Cat index view
+def cat_index(request):
+   cats = Cat.objects.all()  # look familiar?
+   return render(request, 'cats/index.html', {'cats': cats})
+
+# Cat detail view
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)  # get the cat by id
+    return render(request, 'cats/detail.html', {'cat': cat})
+
+
 
 
